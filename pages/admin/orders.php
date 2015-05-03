3128ed06278de155416e30e7172c13d13efb2df8
Lucas Dixon <ldixon@google.com>
2015-05-03 15:31:47 -0400

    fix link to certificate3 to make new images

--- pages/admin/orders.php
+++ pages/admin/orders.php
@@ -10,13 +10,13 @@ $limit = set_default(sql_str_escape($_REQUEST['limit']), 10);
 $date = set_default($_REQUEST['date'], null);
 
 // actions that need full info on a single point
-if($act == "edit-order" or $act == "show-edit-order" 
+if($act == "edit-order" or $act == "show-edit-order"
    or $act == "delete-order" or $act == "attach_thm" or $act == "send_thm") {
   $point_id = set_default($_REQUEST['id'], null);
   $point = get_point($point_id);
 }
 
-// 
+//
 if($act == "attach_thm") {
   if(has_points_related_from($point,'thm.','named.')) {
     $act = "search";
@@ -28,7 +28,7 @@ if($act == "attach_thm") {
     $user = get_user_of_point($point);
     $act_body = "named ". sql_str_escape($point['title'])." by ".sql_str_escape($user['firstname'])." ".sql_str_escape($user['lastname'])." ($email)";
     $thm_point = change_some_point($user[id], "point_type = 'thm.unnamed.'",
-      array('point_type' => "thm.inprocess.", 'title' => $point['title']), 
+      array('point_type' => "thm.inprocess.", 'title' => $point['title']),
       'thm.processing1.', $act_body);
     if($thm_point == null){
       new_msg("Action Failed: there are no un-named theorems!");
@@ -38,26 +38,26 @@ if($act == "attach_thm") {
       $limit = 1;
       $date = null;
     } else {
-      $rel = array('src_obj_id' => $point_id, 
-                   'dst_obj_id' => $thm_point['id'], 
+      $rel = array('src_obj_id' => $point_id,
+                   'dst_obj_id' => $thm_point['id'],
                    'relation_type' => 'named.');
       $rel_id = create_rel($_SESSION['id'], $rel, $act_body);
-      $act_id = edit_point($_SESSION['id'], $point, 'order.new.in_process.',  
+      $act_id = edit_point($_SESSION['id'], $point, 'order.new.in_process.',
         sql_str_escape($point['title']), sql_str_escape($point['body']), 'attached theorem');
       $existing_cert = try_get_point_related_from($point,"certificate.","has_certificate.");
       if($existing_cert == null) {
         $cer_title = $point_id.md5($point['title']);
       } else {
-        $cer_title = $existing_cert['title'];       
+        $cer_title = $existing_cert['title'];
       }
       $cer = create_point($user[id], "certificate.", $cer_title," ");
-      $rel2 = array('src_obj_id' => $point_id, 
-                   'dst_obj_id' => $cer, 
+      $rel2 = array('src_obj_id' => $point_id,
+                   'dst_obj_id' => $cer,
                    'relation_type' => 'has_certificate.');
-       
+
       $cer_act_body = "Certificate ". $cer_title." of theorem ".sql_str_escape($point['title']);
       $rel_id2 = create_rel($_SESSION['id'], $rel2, $cer_act_body);
-      
+
       // this is to show exacyly one order/point after we o the edit
       $act = "search";
       $search="p.id='$point_id'";
@@ -66,7 +66,7 @@ if($act == "attach_thm") {
     }
   }
 }
-  
+
 if($act == "send_thm") {
   if(has_points_related_from($point,'thm.named.','named.')) {
     $act = "search";
@@ -76,14 +76,14 @@ if($act == "send_thm") {
     new_msg("theorem appears to already have been sent");
   } else {
     $thm_point = get_point_related_from($point,'thm.inprocess.','named.');
-    
+
     $user = get_user_of_point($point);
-    $act_body = "sending theorem: ". sql_str_escape($thm_point['title']) 
+    $act_body = "sending theorem: ". sql_str_escape($thm_point['title'])
       . " (" . $thm_point['id'] . ")";
-    
+
     $thm_point = edit_point($_SESSION['id'], $thm_point, 'thm.named.',
       sql_str_escape($thm_point['title']), sql_str_escape($thm_point['body']), $act_body);
-    $act_id = edit_point($_SESSION['id'], $point, 'order.hasthm.',  
+    $act_id = edit_point($_SESSION['id'], $point, 'order.hasthm.',
       sql_str_escape($point['title']), sql_str_escape($point['body']), $act_body);
     $act = "search";
     $search="p.id='$point_id'";
@@ -97,12 +97,12 @@ if($act == "send_thm") {
          'orderid' => $point['id'],
          //certificate_id'=>($point['id'].md5($point['title'])));
          'certificate_id'=>($cert['title']));
-   
+
     $selected_radio = $_POST['lang_email'];
     $emailfile = 'pages/email/order/user_fulfilled_email.'.$selected_radio.'.php';
     $message = email_of_phpfile( $emailfile, $email_vals);
     send_email( $user['email'], 'TheoryMine : Discovered ' .  $point['title'], $message);
-    
+
     $message2 = email_of_phpfile('pages/email/order/us_fulfilled_email.php', $email_vals);
     send_email($admin_email, 'TheoryMine : ORDER FULFILLED ', $message2);
   }
@@ -118,14 +118,14 @@ if($act == "edit-order" or $act == "make-new-order"){
   $order_status = set_default(sql_str_escape($_REQUEST['order_status']), null);
 
   if($act == "edit-order"){
-    // edit the theorem 
+    // edit the theorem
     edit_point($user_id, $point, $order_status, $order_name, $order_body, $abody);
     $act='search';
     $search="p.id='$point_id'";
     $limit = 1;
     $date = null;
   }
-  
+
   if($act == "make-new-order"){
     $point_id = create_point($user_id, $order_status, $order_name, $order_body, $abody);
     $point = get_point($point_id);
@@ -150,7 +150,7 @@ if($act == "delete-order"){
     order body: <? print $point['body']; ?><br>
     order status: <? print $point['point_type']; ?><br>
     history_id: <? print $point['history_id']; ?><br>
-    action_id: <? print $point['action_id']; ?><br> 
+    action_id: <? print $point['action_id']; ?><br>
     time stamp: <? print $point['time_stamp']; ?>
     </div>
     <?
@@ -187,9 +187,9 @@ if($msgs != null){
   <form action="?go=admin&s=orders" method="post">
   <input type="hidden" name="act" value="search" size="70">
   <b>Search:</b> <input type="text" name="search" value="<? print $search; ?>" size="70"><br>
-  Offset: 
+  Offset:
   <input type="text" name="offset" value="<? print $offset; ?>" size="10">; Limit: <input type="text" name="limit" value="<? print $limit; ?>" size="10">
-  Creation Date: <input type="text" name="date" value="<? print $date; ?>" size="20">  
+  Creation Date: <input type="text" name="date" value="<? print $date; ?>" size="20">
   <input class="greenbutton" type="submit" value="Search!"> &nbsp; <a class="greenbutton" href="?go=admin&s=orders">Show All</a><br>
   SQL added to WHERE e.g. <code>p.id = '3'</code> for finding points with id of 3, <code>p.title >= 'pants'</code> for finding all points where the title contains the substring 'pants'.
   </form>
@@ -206,7 +206,7 @@ if($act == 'search') {
   }
   $res = get_from_points_and_actions_and_user($search, "AND p.point_type LIKE 'order.%'", $offset, $limit);
   $rows = $res['rows'];
-  
+
   if($rows != null) { ?>
     <h3>Found <? print($res['rowcount']); ?> Points:</h3>
     <div class="simple-border">
@@ -215,15 +215,15 @@ if($act == 'search') {
     $fst = true;
     foreach($rows as $point) {
       $toggle = !$toggle;
-      if($fst){ $fst = false; 
+      if($fst){ $fst = false;
         ?><div class="simple-list0"><?
-      } else if($toggle){ 
+      } else if($toggle){
         ?><div class="simple-list1"><?
       } else {
         ?><div class="simple-list2"><?
       } ?>
       <div class="edit-btns-right"><a href="?go=admin&s=orders&act=show-edit-order&id=<? print $point['id']; ?>">edit</a> | <a href="?go=admin&s=orders&search=<? print urlencode("p.id='" . $point['id'] . "'"); ?>&limit=1">show/process</a></div>
-      
+
 
       id: <? print $point['id']; ?>; Order to name a theorem: "<? print $point['title']; ?>"<br>
       date: <?print $point['time_stamp']?> order status: <? print $point['point_type']; ?>
@@ -237,7 +237,7 @@ if($act == 'search') {
         $cert =  get_point_related_from($point,'certificate.','has_certificate.');
         if($cert == null) {
           print "something is wrong with certid";
-        } 
+        }
         else{
           $thm_file_dir="certificates/".($cert['title'])."/thm.jpg";
           $thy_file_dir="certificates/".($cert['title'])."/thy.jpg";
@@ -265,8 +265,8 @@ if($act == 'search') {
           </font>
           <?
         }
-        
-       
+
+
       } else if(subtype($point['point_type'],'order.new.in_process.')) {
       ?>[ In Process ]
       <?
@@ -275,7 +275,7 @@ if($act == 'search') {
       }
       ?>
       <br>
-      <? 
+      <?
       if($res['rowcount'] == 1 and $limit == 1) {
         $user = get_user_of_point($point);
 
@@ -287,37 +287,37 @@ if($act == 'search') {
         </form>
         <?
       } else if(subtype($point['point_type'],'order.new.in_process.')) {
-        /* FIXME: 2 rpoblems: 
+        /* FIXME: 2 rpoblems:
         1. $point['id'].md5($point['title']) is a key that can easily be spoofed. Better to use: $point['id'].md5($point['id'].$point['title'])
-        2. when a certificate is attached, we should lookup the certificate point, and use it's title. We should not recompute the hash, as recomputin the hash means we can't change the way the hash is computed. 
+        2. when a certificate is attached, we should lookup the certificate point, and use it's title. We should not recompute the hash, as recomputin the hash means we can't change the way the hash is computed.
         */
         $cert =  get_point_related_from($point,"certificate.","has_certificate.");
 
         ?><form action="?go=admin&s=orders" method="post">
-        
+
         <input type="radio" name="lang_email" value="en"  checked /> English<br />
         <input type="radio" name="lang_email" value="cn" /> Chinese<br />
         <input type="radio" name="lang_email" value="sp" /> Spanish<br />
-       
+
         <input type="hidden" name="act" value="send_thm">
         <input type="hidden" name="id" value="<? print($point['id']); ?>">
-        <input class="greenbutton" type="submit" value="Send Theorem"> 
-        <a href="?go=certificate&pid=<? 
+        <input class="greenbutton" type="submit" value="Send Theorem">
+        <a href="?go=certificate&pid=<?
         // print ($point['id'].md5($point['title']));
-          print ($cert['title']); 
+          print ($cert['title']);
           ?>">
-          View Certificate</a>  |  
-            <a href="?go=certificate_special&pid=<? 
+          View Certificate</a>  |
+            <a href="?go=certificate_special&pid=<?
          // print ($point['id'].md5($point['title']));
-          print ($cert['title']); 
+          print ($cert['title']);
           ?>">
           Fresh Certificate</a> |
-          <a href="?go=admin&s=certificate2&pid=<? 
-          print ($cert['title']); 
+          <a href="?go=admin&s=certificate3&pid=<?
+          print ($cert['title']);
           ?>">
-          Make Images</a>  
-          
-           
+          Make Images</a>
+
+
         </form>
         <?
       } else if(subtype($point['point_type'],'order.hasthm.')) {
@@ -327,31 +327,31 @@ if($act == 'search') {
         <a href="?go=certificate&pid=<? print (//$point['id'].md5($point['title']));
         $cert['title']);
         ?>"> View Certificate</a>  |
-            <a href="?go=certificate_special&pid=<? 
+            <a href="?go=certificate_special&pid=<?
           //print ($point['id'].md5($point['title']));
-         print ($cert['title']); 
+         print ($cert['title']);
           ?>&fresh=yes">
           Fresh Certificate</a>|
-        <a href="?go=admin&s=certificate2&pid=<? 
-          print ($cert['title']); 
+        <a href="?go=admin&s=certificate3&pid=<?
+          print ($cert['title']);
           ?>">
-          Make Images</a>  
+          Make Images</a>
           <br/><?
       }
         ?>Order body (paypal txn id): <span class="simple-block"><? print $point['body']; ?></span><br>
         ordered by user (<? print $user['id']; ?>)
-        <? print $user['firstname']; ?> <? print $user['lastname']; ?>,  
-        <? print $user['email']; ?><br> 
+        <? print $user['firstname']; ?> <? print $user['lastname']; ?>,
+        <? print $user['email']; ?><br>
         history_id: <? print $point['history_id']; ?><br>
         prev_id: <? print $point['prev_id']; ?><br>
-        action_id: <? print $point['action_id']; ?><br> 
-        time_stamp: <? print $point['time_stamp']; ?><br> 
-        creation_time: <? print $point['creation_date']; ?><br> 
-        action_type: <? print $point['action_type']; ?>; action_timestamp: <? print $point['a_time_stamp']; ?>; 
-        action_body: <? print $point['action_body']; ?>; ipaddr: <? print $point['ipaddr']; ?><br> 
-        user_id: <? print $point['user_id']; ?>; firstname: <? print $point['firstname']; ?>; lastname: <? print $point['lastname']; ?>; email: <? print $point['email']; ?>; 
-        last_act_time: <? print $point['last_act_time']; ?>;  
-        last_act_kind: <? print $point['last_act_kind']; ?><br> 
+        action_id: <? print $point['action_id']; ?><br>
+        time_stamp: <? print $point['time_stamp']; ?><br>
+        creation_time: <? print $point['creation_date']; ?><br>
+        action_type: <? print $point['action_type']; ?>; action_timestamp: <? print $point['a_time_stamp']; ?>;
+        action_body: <? print $point['action_body']; ?>; ipaddr: <? print $point['ipaddr']; ?><br>
+        user_id: <? print $point['user_id']; ?>; firstname: <? print $point['firstname']; ?>; lastname: <? print $point['lastname']; ?>; email: <? print $point['email']; ?>;
+        last_act_time: <? print $point['last_act_time']; ?>;
+        last_act_kind: <? print $point['last_act_kind']; ?><br>
         <?
         $row = try_get_row("SELECT * FROM $db_paypal_payment_info WHERE txn_id = '" . sql_str_escape($point['body']) . "'");
         if($row == null) {
@@ -375,7 +375,7 @@ if($act == 'search') {
           'txn_id',
           'txn_type',
           'verify_sign',
-          // Buyer Information 
+          // Buyer Information
           'address_country',
           'address_city',
           'address_country_code',
@@ -389,7 +389,7 @@ if($act == 'search') {
           'payer_business_name',
           'payer_email',
           'payer_id',
-          // Payment Information 
+          // Payment Information
           'auth_amount',
           'auth_exp',
           'auth_id',
@@ -424,13 +424,13 @@ if($act == 'search') {
           'shipping_method',
           'tax',
           'transaction_entity');
-          
+
           foreach($paypal_vars as $k) {
             print($k . ": " . $row[$k] . "<br />");
           }
         }
         ?>
-        <? 
+        <?
       }
       //print_r($point);
       ?>
@@ -451,9 +451,9 @@ if($act == 'search') {
 
 
 if($act == "show-edit-order"){
-  if($point['user_id'] == null or $point['user_id'] == ""){ 
-    $point['user_id'] = $_SESSION['id']; 
-  } 
+  if($point['user_id'] == null or $point['user_id'] == ""){
+    $point['user_id'] = $_SESSION['id'];
+  }
   ?>
   <p>
 
@@ -495,15 +495,15 @@ if($act == "show-edit-order"){
   <input class="greenbutton" type="submit" value="Save changes"> &nbsp;&nbsp; <a class="redbutton" href="?go=admin&s=orders&act=search&search=<? print urlencode("p.id='" . $point['id'] . "'"); ?>">Cancel</a></td></tr>
   </table>
   </form>
-  
+
   <h4>Other actions</h4>
   <!-- <br><br>
   To update the last-change timestamp to now:<br>
-  <a class="greenbutton" href="?go=admin&s=orders&act=touch&obj_id=<? print($point['id']); ?>" method="post">Update time</a> 
+  <a class="greenbutton" href="?go=admin&s=orders&act=touch&obj_id=<? print($point['id']); ?>" method="post">Update time</a>
   <br><br> -->
-  To <span class="warning">delete</span> the order: 
+  To <span class="warning">delete</span> the order:
   <a class="redbutton" href="?go=admin&s=orders&act=delete-order&id=<? print($point['id']); ?>" method="post">Delete order</a>
-  
+
   </div>
   <?
 } else {
@@ -511,9 +511,9 @@ if($act == "show-edit-order"){
 }
 
 if($act == "enter-new-order"){
-  if($user_id == null or $user_id == ""){ $user_id = $_SESSION['id']; } 
+  if($user_id == null or $user_id == ""){ $user_id = $_SESSION['id']; }
   ?>
-  <p> 
+  <p>
   <div class="simple-block">
   <h3> New Order Details </h3>
   <form action="?go=admin&s=orders" method="post">
@@ -554,7 +554,7 @@ if($act == "enter-new-order"){
   </div>
   <?
 } else {
-  ?><p><a href="?go=admin&s=orders&act=enter-new-order">Make a new order</a></p> 
+  ?><p><a href="?go=admin&s=orders&act=enter-new-order">Make a new order</a></p>
 <?
 }
 ?>
