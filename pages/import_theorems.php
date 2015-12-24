f0a29f6f3ddeced69c9f48a2483a99d1bb96ea2b
Lucas Dixon <lucas.dixon@gmail.com>
2015-12-24 10:50:43 -0500

    cleanup of spaces

--- pages/import_theorems.php
+++ pages/import_theorems.php
@@ -12,9 +12,9 @@ function fix_bad_init_file_contents($contents) {
   $contents = preg_replace('/([\\(, ])([b])([\\), \\n\\<\\\'])/',
     '\\1y\\3',$contents);
   $contents = preg_replace('/([\\(, ])([c])([\\), \\n\\<\\\'])/',
-    '\\1z\\3',$contents);      
+    '\\1z\\3',$contents);
   $contents = preg_replace('/([\\(, ])([d])([\\), \\n\\<\\\'])/',
-    '\\1w\\3',$contents); 
+    '\\1w\\3',$contents);
   return $contents;
 }
 
@@ -24,18 +24,20 @@ if($_REQUEST['act']=="upload"){
   if ($_FILES['theorems_file']['error'] > 0) {
     print("Error: " . $_FILES['theorems_file']['error'] . "<br />");
   } else {
-    $uploaded_name = $thy_name_prefix . "-" .  basename($_FILES['theorems_file']['name'],".php");
+    $uploaded_name = $thy_name_prefix . "-" .
+        basename($_FILES['theorems_file']['name'],".php");
     $tmp_fname = $_FILES['theorems_file']['tmp_name'];
-    
+
     if($handle = fopen($tmp_fname, 'r')) {
       $contents = fread($handle, filesize($tmp_fname));
       fclose($handle);
       $contents_orig = $contents;
       $contents = fix_bad_init_file_contents($contents);
       eval($contents);
-  
+
       if(sizeof($theorems) > 0) {
-        $point = try_get_row("SELECT p.* FROM $db_points as p WHERE p.body = '" . sql_str_escape($theory) . "'");
+        $point = try_get_row("SELECT p.* FROM $db_points as p WHERE p.body = '"
+            . sql_str_escape($theory) . "'");
         if($point == null) {
           $thy_point_id = create_point(1, 'thy.unnamed.',
             sql_str_escape($uploaded_name), sql_str_escape($theory), 'imported.');
@@ -43,19 +45,19 @@ if($_REQUEST['act']=="upload"){
           $abody = 'imported.' . $thy_point_id;
           foreach($theorems as $thm) {
             $i ++;
-            $thm_id = create_point(1, 'thm.unnamed.', 
+            $thm_id = create_point(1, 'thm.unnamed.',
               $uploaded_name . '-' . $i, $thm['statement'], $abody);
-        
-            $proof_id = create_point(1, 'proof.', 
+
+            $proof_id = create_point(1, 'proof.',
               $thm['statement'], $thm['proof'], $abody);
-        
-            $proof_rel = array('src_obj_id' => $proof_id, 
-                           'dst_obj_id' => $thm_id, 
+
+            $proof_rel = array('src_obj_id' => $proof_id,
+                           'dst_obj_id' => $thm_id,
                            'relation_type' => 'proof.');
             $proof_rel_id = create_rel(1, $proof_rel, $abody);
-        
-            $inthy_rel = array('src_obj_id' => $thm_id, 
-                           'dst_obj_id' => $thy_point_id, 
+
+            $inthy_rel = array('src_obj_id' => $thm_id,
+                           'dst_obj_id' => $thy_point_id,
                            'relation_type' => 'inthy.');
             $inthy_rel_id = create_rel(1, $inthy_rel, $abody);
           }
@@ -77,9 +79,9 @@ if($_REQUEST['act']=="upload"){
       ?></div>
       then:<ul><?
       $i = 0;
-      foreach($theorems as $thm) { 
+      foreach($theorems as $thm) {
         $i ++;
-        
+
         ?>
         <li><div class="theorem-statement"><? print($uploaded_name . '-' . $i); ?>: <? print($thm['statement']); ?></div>
             <div class="theorem-proof">Proof outline: <? print($thm['proof']); ?></div>
@@ -91,13 +93,13 @@ if($_REQUEST['act']=="upload"){
       ?><div class="warning">fopen failed<? print($_FILES['theorems_file']['tmp_name']); ?></div><?
     }
   }
-}  
+}
 ?><p>Upload: <? print($_FILES['theorems_file']['name']); ?><br />
 Type: <? print($_FILES['theorems_file']['type']); ?> <br />
 Size: <? print($_FILES['theorems_file']['size'] / 1024); ?> Kb<br />
 Stored in: <? print($_FILES['theorems_file']['tmp_name']); ?> <br />
 </p>
-<div class="code"><?  
+<div class="code"><?
       print(htmlentities($contents_orig));
 ?></div><?
 
